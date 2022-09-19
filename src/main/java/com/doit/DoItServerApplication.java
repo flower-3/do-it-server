090cfaf8f09e28e60e9455196e601fc1de2d4c7c
junam2 <speedlin4584@gmail.com>
2022-09-19 23:50:16 +0900

    Initialize Spring Boot

---
+++ src/main/java/com/doit/DoItServerApplication.java
@@ -0,0 +1,13 @@
+package com.doit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoItServerApplication.class, args);
+    }
+
+}
