dd6621137ad95c3517a85c084d2012905a86b286
junam2 <speedlin4584@gmail.com>
2022-10-10 18:34:03 +0900

    chore: requestMapping 설정

--- src/main/java/com/doit/controller/TestController.java
+++ src/main/java/com/doit/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.doit.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api")
 @RestController
 public class TestController {
 
