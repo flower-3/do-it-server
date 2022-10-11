fe75d7266a098fa7e0566dc464055ba03ff24c69
박영운[주문형페이개발] <youngun.park@navercorp.com>
2022-10-12 01:00:01 +0900

    123

--- deploy.sh
+++ deploy.sh
@@ -36,4 +36,4 @@ echo "do-it-server-${TERMINATE_CONTAINER} down"
 sudo docker-compose -p do-it-server-${TERMINATE_CONTAINER} down
 sudo docker image prune -f
 
-echo "$(awk -F- '{print $3}' <<< ${EXIST_RED})"
+echo "$(awk -F- '{print $4}' <<< '${EXIST_RED}')"
